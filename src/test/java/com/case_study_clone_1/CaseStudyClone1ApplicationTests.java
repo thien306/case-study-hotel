ae2be584229ef13404cecb416a976b537c6b4093
NhatHUY0910 <khuongduy33712@gmail.com>
2024-06-06 14:45:14 +0700

    Init project

---
+++ src/test/java/com/case_study_clone_1/CaseStudyClone1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_study_clone_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyClone1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
