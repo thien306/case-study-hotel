3e53d937e7fe029c6499d767b4d117ffd660f37b
thien306 <nguyenvantho306@gmail.com>
2024-06-07 16:03:41 +0700

    solve conflict

--- src/main/java/com/codegym/security/JwtTokenProvider.java
+++ src/main/java/com/codegym/security/JwtTokenProvider.java
@@ -1,14 +1,18 @@
 package com.codegym.security;
 
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
-import java.security.SignatureException;
 import java.util.Date;
 
 @Component
@@ -57,6 +61,8 @@ public class JwtTokenProvider {
         try {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
+        }catch (SignatureException ex) {
+            logger.error("Invalid JWT signature");
         } catch (MalformedJwtException ex) {
             logger.error("Invalid JWT token");
         } catch (ExpiredJwtException ex) {
