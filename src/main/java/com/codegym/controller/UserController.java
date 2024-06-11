b88a9602151af4eeae843b91175625e9201fff25
NhatHUY0910 <khuongduy33712@gmail.com>
2024-06-11 10:16:44 +0700

    refactor codes, implementing update password api

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/users")
@@ -22,5 +19,11 @@ public class UserController {
     public ResponseEntity<?> saveUser(@RequestBody User user){
         userService.save(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/update-password")
+    public ResponseEntity<?> updatePassword(){
+        System.out.println("hello");
+        return null;
     }
 }
